6a04bafa6a3cf112ec6efae2f7a7993ec4bf918c
Nicodemus Karisa <nkarisa@ke.ci.org>
2019-04-15 22:45:08 +0300

    Added a datatable

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Welcome to Git | Git Branching Lesson 1</title>
+	<title>Welcome to Git | Git Branching</title>
 	<link rel="stylesheet" href="assets/css/bootstrap.css">
 	
 	<script src="assets/js/jquery-3.3.1.min.js"></script>
@@ -20,11 +20,36 @@
 	
 	<div class="row" style="margin-top: 20px">
 		<div class="col-xs-offset-1 col-xs-10 col-xs-offset-1">
-<<<<<<< HEAD
-			Welcome to Git. This is a Git branching lesson.
-=======
 			Welcome to Git	
->>>>>>> hotfix
+			<p></p>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Day 1</th>
+						<th>Day 2</th>
+						<th>Day 3</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>This</td>
+						<td>is</td>
+						<td>Day</td>
+					</tr>
+					
+					<tr>
+						<td>One</td>
+						<td>We</td>
+						<td>Must</td>
+					</tr>
+					
+					<tr>
+						<td>Be</td>
+						<td>Very</td>
+						<td>Glad</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 	
