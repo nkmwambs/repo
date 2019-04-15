1c5e08c61779b51d4a7209ccc4f93cb215724cfa
Nicodemus Karisa <nkarisa@ke.ci.org>
2019-04-15 20:43:19 +0300

    Index and Asssets added

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Welcome to Git</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
+	<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+	
+</head>
+<body class="page-body">
+
+<div id="page-container">
+	
+	<div class="row" style="margin-top: 20px">
+		<div class="col-xs-offset-1 col-xs-10 col-xs-offset-1">
+			Welcome to Git	
+		</div>
+	</div>
+	
+</div>
+
+</body>
+</html>
